e1493fbe1111130060d5d83ad2bffb51e1fb94dc
gnurmatova <gulanurmatova@Gulas-MacBook-Pro.local>
2014-09-01 15:56:04 -0400

    Adding Range

---
+++ lecture1/Range.py
@@ -0,0 +1,11 @@
+print("for number in range(1,5):")
+for number in range(1,5):
+    print(number)
+
+print("for number in range(5):")
+for number in range(5):
+    print(number)
+
+print("for number in range(1,13,4):")
+for number in range(1,13,4):
+    print(number)
