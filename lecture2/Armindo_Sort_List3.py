3b23b360306c180930160f81b692c81c147a8d6c
gnurmatova <gulanurmatova@Gulas-MacBook-Pro.local>
2014-09-14 14:26:18 -0400

    functions and Armindo's sort examples

--- lecture2/Armindo_Sort_List3.py
+++ lecture2/Armindo_Sort_List3.py
@@ -1,9 +1,18 @@
 print("\nAlphabetic Example 2")
 names = [["Kim", "Mike", "Alex", "Bob"],["Jim", "John", "Mary"],["Joe", "Tom", "Amy"]]
+
+print("initial list:")
+print(names)
+print("_____________")
+
 sortedNames = []
 for n in names:
     n.sort()
     sortedNames.append(n)
 
+print("each sub-list is sorted individually:")
 print(sortedNames)
+print("_____________")
+
+print("entire list is sorted at once:")
 print(sorted(sortedNames))
