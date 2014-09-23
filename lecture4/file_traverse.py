f7886f7f81843a1f727ca4e2a2aaa2863cc93b6b
gnurmatova <gulanurmatova@Gulas-MacBook-Pro.local>
2014-09-23 19:51:35 -0400

    file traverse

--- lecture4/file_traverse.py
+++ lecture4/file_traverse.py
@@ -6,6 +6,5 @@ for dirpath, dirs, files in os.walk(start_dir):
 	for single_file in files:
 		if fnmatch.fnmatch(single_file, "*log"):
 			print("Deleting ", single_file)
-			print(os.path.join(dirpath, single_file))
 			os.remove(os.path.join(dirpath, single_file))
 
