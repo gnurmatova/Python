164669fc4cc0b0be7f216eb5f0580d3bb2d20644
gnurmatova <gulanurmatova@Gulas-MacBook-Pro.local>
2014-11-02 08:15:21 -0500

    lecture9

---
+++ lecture9/webapp/zoo.py
@@ -0,0 +1,14 @@
+from wsgiref.simple_server import make_server
+
+def hello_world_app(environ, start_response):
+	status = '200 OK'
+	headers = [('Content-type', 'html; charset=utf-8')]
+	start_response(status, headers)
+	message = "<h1>Welcome to the Zoo</h1>"
+	return[bytes(message,'utf-8')]
+
+httpd = make_server('', 8000, hello_world_app)
+print("Serving on port 8000...")
+
+
+httpd.serve_forever()
